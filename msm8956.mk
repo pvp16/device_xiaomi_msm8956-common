4f42900a71e6f9bd5fce35319323946ef0aa2ef0
Offain <fabian.leutenegger@bluewin.ch>
2017-08-21 06:34:16 +0000

    msm8956-common: add QPerformance.jar to boot jars and pinner service
    * add QPerformance to boot jars
    * add QPerformance to the Pinner configuration to ensure the best latency for the userspace boosting framework.

--- msm8956.mk
+++ msm8956.mk
@@ -272,6 +272,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
     power.msm8952
 
+# QPerformance
+PRODUCT_BOOT_JARS += QPerformance
+
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
     libtinyxml \
